a7e2f019dcac017fa781f6f04576510a2ba401d0
anwar <mac.mem65@gmail.com>
2020-05-29 02:19:15 +0200

    Renabled Strong migrations + update psql in travis

--- db/migrate/20200510160009_add_trackable_to_users.rb
+++ db/migrate/20200510160009_add_trackable_to_users.rb
@@ -1,7 +1,7 @@
 class AddTrackableToUsers < ActiveRecord::Migration[6.0]
   def change
     ## Trackable
-    add_column :users, :sign_in_count, :integer, default: 0, null: false
+    add_column :users, :sign_in_count, :integer
     add_column :users, :current_sign_in_at, :datetime
     add_column :users, :last_sign_in_at, :datetime
     add_column :users, :current_sign_in_ip, :inet
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_28_173136) do
+ActiveRecord::Schema.define(version: 2020_05_29_001111) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,7 +83,7 @@ ActiveRecord::Schema.define(version: 2020_05_28_173136) do
     t.json "tokens"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "sign_in_count", default: 0, null: false
+    t.integer "sign_in_count"
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.inet "current_sign_in_ip"
