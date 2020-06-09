6c69b2c3949462c7954f7553891a24eda62211c9
anwar <mac.mem65@gmail.com>
2020-06-09 03:39:58 +0200

    updated email cred

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,3 +1,14 @@
+ActionMailer::Base.smtp_settings = {
+    :user_name => 'apikey',
+    :password => ENV["SEND_GRID_PASSWORD"],
+    :domain => 'lab-3.com',
+    :address => 'smtp.sendgrid.net',
+    :port => 465,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+}
+
+
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
