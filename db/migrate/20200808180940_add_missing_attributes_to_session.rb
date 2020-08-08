f3e4532881cb3a5fbb4f66f5c08fb19944b34940
anwar <mac.mem65@gmail.com>
2020-08-08 20:25:36 +0200

    fixed migrations warnings

--- db/migrate/20200808180940_add_missing_attributes_to_session.rb
+++ db/migrate/20200808180940_add_missing_attributes_to_session.rb
@@ -1,7 +1,8 @@
 class AddMissingAttributesToSession < ActiveRecord::Migration[6.0]
   def change
-    add_column :sessions, :apply_checks, :boolean, default: true
+    add_column :sessions, :apply_checks, :boolean
     add_column :sessions, :lat, :float
     add_column :sessions, :long, :float
+    change_column_default :sessions, :apply_checks, true
   end
 end
