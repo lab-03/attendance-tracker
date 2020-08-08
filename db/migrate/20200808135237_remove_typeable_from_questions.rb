5323396864ed54527207d52e63d5a1a3cf8c8b58
anwar <mac.mem65@gmail.com>
2020-08-08 20:20:00 +0200

    removed unused migration

--- db/migrate/20200808135237_remove_typeable_from_questions.rb
+++
@@ -1,8 +0,0 @@
-class RemoveTypeableFromQuestions < ActiveRecord::Migration[6.0]
-  def change
-    safety_assured do
-      remove_column :questions, :typeable_type, :string
-      remove_column :questions, :typeable_id, :integer
-    end
-  end
-end
