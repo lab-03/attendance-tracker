69f3483b036813069cca5ea9d8842676c25af5ec
anwar <mac.mem65@gmail.com>
2020-08-09 11:39:30 +0200

    fixed migrations warnings

--- db/migrate/20200807105345_add_types_to_questions.rb
+++ db/migrate/20200807105345_add_types_to_questions.rb
@@ -1,7 +1,10 @@
 class AddTypesToQuestions < ActiveRecord::Migration[6.0]
   def change
-    add_column :questions, :is_rating, :boolean, default: false
-    add_column :questions, :is_text, :boolean, default: false
-    add_column :questions, :is_boolean, :boolean, default: false
+    add_column :questions, :is_rating, :boolean
+    add_column :questions, :is_text, :boolean
+    add_column :questions, :is_boolean, :boolean
+    change_column_default :questions, :is_rating, false
+    change_column_default :questions, :is_text, false
+    change_column_default :questions, :is_boolean, false
   end
 end
