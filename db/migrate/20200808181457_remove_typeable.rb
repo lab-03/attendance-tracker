66d85e0659f36c32b07dd027a717976fcd46e6f2
anwar <mac.mem65@gmail.com>
2020-08-09 11:54:20 +0200

    removed duplicate migration

--- db/migrate/20200808181457_remove_typeable.rb
+++
@@ -1,8 +0,0 @@
-class RemoveTypeable < ActiveRecord::Migration[6.0]
-  def change
-    safety_assured do
-      remove_column :questions, :typeable_id
-      remove_column :questions, :typeable_type
-    end
-  end
-end
