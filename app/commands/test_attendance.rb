1d6926424e672961c410c99a0f5b8577cba88fae
anwar <mac.mem65@gmail.com>
2020-08-14 23:44:46 +0200

    load test controller

--- app/commands/test_attendance.rb
+++ app/commands/test_attendance.rb
@@ -16,8 +16,7 @@ class TestAttendance < CommandBase
 
 
   def verification_params
-    {hash: "test", latitude: 1.1, longitude: 2.2, original_face: "https://bit.ly/3fJesao",
-     captured_face: "https://bit.ly/33F93Pq"}
+    {hash: "test", latitude: 1.1, longitude: 2.2}
   end
 
 end
